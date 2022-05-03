4119fe89e683658f8b01aefd53ea34919e9506a0
jeonisje <96jje@hanmail.net>
2022-05-03 17:37:13 +0900

    studying

---
+++ myapp/lib/src/main/java/com/myapp/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,56 @@
+package com.myapp.book.springboot.service.posts;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.myapp.book.springboot.domain.posts.Posts;
+import com.myapp.book.springboot.domain.posts.PostsRepository;
+import com.myapp.book.springboot.web.dto.PostsListResponseDto;
+import com.myapp.book.springboot.web.dto.PostsResponseDto;
+import com.myapp.book.springboot.web.dto.PostsSaveRequestDto;
+import com.myapp.book.springboot.web.dto.PostsUpdateRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+	private final PostsRepository postsRepository;
+	
+	@Transactional
+	public Long save(PostsSaveRequestDto requestDto) {
+		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+	
+	@Transactional
+	public Long update(Long id, PostsUpdateRequestDto requestDto) {
+		Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+		posts.update(requestDto.getTitle(), requestDto.getContent());
+		
+		return id;
+	}
+	
+	public PostsResponseDto findById(Long id) {
+		Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+		
+		return new PostsResponseDto(entity);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<PostsListResponseDto> findAllDesc() {
+		return postsRepository.findAllDesc().stream()
+				.map(PostsListResponseDto::new)
+				.collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		Posts posts = postsRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("There is no such post. id=" + id));
+		postsRepository.delete(posts);
+	}
+}
